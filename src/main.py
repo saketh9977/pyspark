8975710cb0d2ed9289057d7e734f541ab548a730
Saketh Katari <saketh@Sakeths-MacBook-Pro.local>
2022-08-11 14:42:15 +0530

    added cell-udf

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 
-from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import udf
 
 IN_CSV_FILE = '../data/nse-2021-08-to-2022-08.csv'
 SQL_FILE = './transformations.sql'
+
+@udf(returnType=IntegerType())
+def cell_transformation(cell):
+    return cell + 10
 
 def main():
     spark = SparkSession.builder.getOrCreate()
@@ -16,7 +22,10 @@ def main():
     with open(SQL_FILE, 'r') as stream:
         commands = stream.read()
         df_res = spark.sql(commands)
-    df_res.show()
+
+    # df_res.show()
+    df_cell_t = df_res.withColumn('close_cell_t', cell_transformation('close'))
+    df_cell_t.show()
 
 if __name__ == '__main__':
     main()
